23d4708e8a43182037737e27284ce4e89c94a755
Aaron Roh <roharon98@gmail.com>
2019-11-21 11:18:36 +0900

    feat: update Github_token in GdfUser models
    update Github_token in GdfUser models when github_oauth_callback & GdfUser model modify

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class GdfUser(models.Model):
-    length = 50
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='git')
-    github_token = models.CharField(max_length=length, blank=True)
-    gitlab_token = models.CharField(max_length=length, blank=True)
-    bitbucket_token = models.CharField(max_length=length, blank=True)
+    username = models.CharField(max_length=50, blank=False, unique=True, primary_key=True)
+    github_token = models.CharField(max_length=50, blank=True, unique=True, null=True)
+    gitlab_token = models.CharField(max_length=50, blank=True, unique=True, null=True)
+    bitbucket_token = models.CharField(max_length=50, blank=True, unique=True, null=True)
--- backend/app/views/oauth2_callback.py
+++ backend/app/views/oauth2_callback.py
@@ -3,12 +3,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 #from gitdefender.client import CLIENT_SECRET, CLIENT_ID
+from ..models import GdfUser
 import os
 import requests, json
 
 @api_view(['GET', 'POST'])
 @permission_classes((IsAuthenticated,))
 def oauth2_callback(request):
+    
     """
     :param request:
     :return {status:'', code:''}:
@@ -36,10 +38,22 @@ def oauth2_callback(request):
         res = requests.post(ACCESS_TOKEN_URL, headers=header, params=params)
         user_access_token = res.json()['access_token']
 
-        user_access_body = dict(code=user_access_token)
+        if len(GdfUser.objects.filter(username=request.user)) == 0:
+            user_Gdf = GdfUser.objects.create(username=request.user, github_tokn=user_access_token)
+        else:
+            user_Gdf = GdfUser.objects.get(username=request.user)
+            user_Gdf.github_token = user_access_token
+
+        user_Gdf.save()
+
+        user_access_body = dict(
+            code=user_Gdf.github_token,
+            username=user_Gdf.username
+            )
 
         return Response(user_access_body, status=status.HTTP_200_OK)
+        # 디버깅용
 
     except Exception as e:
         body = {'message': e}
-        return Response(e, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(body, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
