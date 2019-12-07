46bfcbd92a26478dc0cf30e6c5e27212582908e1
youjin <yujin5836@gmail.com>
2019-12-07 14:46:40 +0900

    Add : GetCommitAPI
    get commit api

--- backend/app/views/GetCommitAPI.py
+++ backend/app/views/GetCommitAPI.py
@@ -1,30 +1,50 @@
-from rest_framework import status
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated ,BasePermission ,SAFE_METHODS
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from ..library.get_commit import crawl_commit
-from ..models import GdfUser # username 
-# repo name 도 가져와야함
+#from rest_framework.views import ListAPIView
 
+from knox.auth import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+
+from ..library.get_commit import crawl_commit
+from ..models import GdfUser
 import json
 
-@api_view(['GET', 'POST'])
-@permission_classes((IsAuthenticated,))
+# 로직 보여주기 
+# 응답할 내용을 적어주기
 
-def get_commit(request):
-    try:
-        models_username = request.user.get_username()
-        user_gdf = GdfUser.objects.get(username=models_username)
-        get_commit_instance = GetRepository(user_gdf.github_token)
-        # 커밋내용 가져오기
-        
-        body = dict(
-            get_commit_instance.get_commit
-        )
 
-        return Response(body, status=status.HTTP_200_OK)
-    except Exception as e:
-        body = dict(
+
+    # 클래스 > 모든 인스턴스에서 공유되는 모든 attribute와 메서드를 위한것
+    #클래스 기반 APIView 사용
+   
+  
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+
+def get_commit(request, format=None):
+        try:
+            print("((((((((((((((((()))))))))))))))))")
+            models_username = request.user.get_username()
+            print("111111111111111111-----------------------")
+
+            user_gdf = GdfUser.objects.get(username=models_username)
+            # 유저이름과 일치할때 
+            print("22222222222222222222-----------------------")
+
+            user_tok = "Token " + user_gdf.github_token
+
+            get_commit_instance = crawl_commit(user_tok)
+
+            body = get_commit_instance.get_commit()
+            print(body)
+            return Response(body, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            body = dict(
             message=e
-        )
-        return Response(body, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                        )
+            return Response(body, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
