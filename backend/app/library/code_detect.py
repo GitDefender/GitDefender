d98aa75fae562dffe6a2700d120c4e4aabf1c8a2
Aaron Roh <roharon98@gmail.com>
2019-12-07 17:56:47 +0900

    add: code_detect library

--- backend/app/library/code_detect.py
+++ backend/app/library/code_detect.py
@@ -1,6 +1,7 @@
 import os
 import yara
 import time
+import git
 #from .crawl_tool_base import CrawlTool
 #from app.library.yara_detect.detect_core import DetectCore
 
@@ -15,6 +16,11 @@ class GetCodeDetect():
         self._get_file_list()
         self._config_yara()
 
+        self._git_clone()
+
+    def _git_clone(self):
+        pass
+    
     def _get_file_list(self):
         for top, dirs, files in os.walk('./yara_detect'):
             for nm in files:    
@@ -29,13 +35,41 @@ class GetCodeDetect():
         rules.save("./yara_detect/Log_Keyword")
 
     def yara_callback(self, data):
-        if(data['matches'] == True):
+        if data['matches'] == True:
             print(self.now_file)
+            #print(data)
             print(data['strings'])
+            with open(self.now_file) as f:
+                count = 0
+                print("count",count)
+                try:
+                    file_offset = list(map(lambda x:int(x[0]), data['strings']))
+                    file_line = f.readlines()
+                    for index, fl in enumerate(file_line):
+                        count += len(fl)
+                        min_offset = min(file_offset)
+                        print("offset", file_offset)
+                        print("min_offset", min(file_offset))
+                        if count > min_offset:
+
+                            print(file_line[index-1])
+                            print(fl)
+                            print(file_line[index+1])
+
+                            file_offset.remove(min_offset)
+                            if len(file_offset) == 0:
+                                break
+                        
+                except Exception as e:
+                    print("--FAIL", e)
+                    pass
+            #158을 검출하면 log__log가 나온다
+            # 이런식으로 찾을까
+            print("count", count)
         return yara.CALLBACK_ALL
 
-    def detect(self):
-        rules = yara.load('./yara_detect/Log_Keyword')
+    def detect(self, target_folder):
+        rules = yara.load(target_folder)
 
         for sourcefile in self.file_list:
             self.now_file = sourcefile
@@ -47,7 +81,7 @@ if __name__ == "__main__":
     
     end1 = time.time()
 
-    result = gcd_instance.detect()
+    result = gcd_instance.detect('./yara_detect/Log_Keyword')
 
     end2 = time.time()
 
