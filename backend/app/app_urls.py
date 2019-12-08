c68a626732a07d8116e054012f0f8a8179b2b6d6
youjin <yujin5836@gmail.com>
2019-12-08 16:24:43 +0900

    Update : app_urls
    Update : app_urls

--- backend/app/app_urls.py
+++ backend/app/app_urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from django.conf.urls import url
-from .views import get_commit
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from .views import oauth2, RegistrationAPI, LoginAPI, UserAPI, LogoutView, oauth2_callback, GetRepositoryView
 from .views import get_branch, get_code_detect
+from .views import get_commit
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -22,7 +23,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('get_commit', get_commit, name='commit'),
     path('auth/oauth2', oauth2, name='oauth2'),
     path('auth/register', RegistrationAPI.as_view(), name='register'),
     path('auth/user', UserAPI.as_view(), name='user'),
@@ -30,6 +30,7 @@ urlpatterns = [
     path('auth/logout', LogoutView.as_view(), name='logout'),
     path('auth/github_oauth_callback', oauth2_callback, name='github_oauth_calback'),
     path('get_repository', GetRepositoryView.as_view(), name='repository'),
+    path('get_commit', get_commit, name='commit'),
     path('get_branch', get_branch, name='branch'),
     path('get_code_detect', get_code_detect, name='codedetect'),
     
