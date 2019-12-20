65f9a5f82a8348f5811be3fd62078727aeca42a9
Aaron Roh <roharon98@gmail.com>
2019-12-20 21:19:33 +0900

    remove: Front

--- Front/src/pages/NotFound.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "antd/dist/antd.css";
-
-const NotFound = () => {
-  return <div>NotFound Page</div>;
-};
-
-export default NotFound;
--- Front/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Main } from "./Main";
-export { default as Auth } from "./Auth";
-export { default as Report } from "./Report";
-export { default as NotFound } from "./NotFound";
